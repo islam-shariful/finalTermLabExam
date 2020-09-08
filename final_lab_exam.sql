7b97e2acf6243f787caf8d9b277c74a13b3d9ad4
shariful-islam-sharif <imdshariful171@gmail.com>
2020-09-09 01:35:12 +0600

    UpdatedSqlFIle

--- final_lab_exam.sql
+++ final_lab_exam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2020 at 09:56 AM
+-- Generation Time: Sep 08, 2020 at 09:34 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -28,11 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `jobs` (
+  `job_id` int(11) NOT NULL,
   `company_name` varchar(100) NOT NULL,
   `job_title` varchar(100) NOT NULL,
   `job_location` varchar(100) NOT NULL,
   `salary` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`job_id`, `company_name`, `job_title`, `job_location`, `salary`) VALUES
+(1, 'aiub', 'teacher', 'kuratoli', 100000),
+(2, 'aiub', 'system admin', 'kuratoli', 100000);
 
 -- --------------------------------------------------------
 
@@ -41,6 +50,7 @@ CREATE TABLE `jobs` (
 --
 
 CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `type` varchar(50) NOT NULL,
@@ -53,19 +63,42 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `type`, `employee_name`, `company_name`, `contact_number`) VALUES
-('admin', 'admin', 'Admin', '', '', ''),
-('employee', 'employee', 'Employee', 'employee', 'aiub', '01632466063');
+INSERT INTO `user` (`id`, `username`, `password`, `type`, `employee_name`, `company_name`, `contact_number`) VALUES
+(1, 'admin', 'admin', 'Admin', '', '', ''),
+(2, 'employee', 'employee', 'Employee', 'employee', 'aiub', '01632466063'),
+(7, 'employee2', 'employee', 'employee', 'employee2', 'AIUB', '01632466063');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`job_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
